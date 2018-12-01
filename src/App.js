47a6acf18a19ed3d9169b1e6873312788b7dcb20
rzhekova <rzhekova@gmail.com>
2018-12-01 14:37:27 +0000

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import RedirectToSearch from "./components/RedirectToSearch";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Switch>
+          <Route path="/asset/:id" render={() => <h2>bye</h2>} />
+          <Route path="/search" render={() => <h2>Hi</h2>} />
+          <Route exact path="/" component={RedirectToSearch} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/RedirectToSearch.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+const RedirectToSearch = () => {
+  return <Redirect to="/search" />;
+};
+
+export default RedirectToSearch;
