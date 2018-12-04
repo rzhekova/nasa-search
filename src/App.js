f56c64c7b9c943141545fa256b4626f5c7a52d5a
rzhekova <rzhekova@gmail.com>
2018-12-04 14:05:39 +0000

    creates build

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route path="/asset/:id" component={AssetPage} />
+          <Route exact path="/" component={RedirectToSearch} />
           <Route
             path="/search"
             render={() =>
@@ -39,7 +40,6 @@ class App extends Component {
               )
             }
           />
-          <Route exact path="/" component={RedirectToSearch} />
         </Switch>
       </div>
     );
@@ -75,8 +75,8 @@ class App extends Component {
           })
         )
         .catch(error => console.log(error));
-    } else {
       alert("Please enter your search terms and select media types");
+    } else {
     }
   };
 
